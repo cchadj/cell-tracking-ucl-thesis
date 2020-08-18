Fixed a bug in ImageDataset that would standardize an image twice giving wrong results. Also implemented object inheritance to prevent future mistakes

generate_datasets.py
@@ -27,6 +27,7 @@ def clean_folder(folder):
 
 
 def create_cell_and_no_cell_patches(
+        video_sessions=None,
         patch_size=(21, 21),
         temporal_width=0,
         normalize=False,
@@ -47,7 +48,10 @@ def create_cell_and_no_cell_patches(
     cell_images_marked = np.zeros_like(cell_images, dtype=np.uint8)
     non_cell_images_marked = np.zeros_like(cell_images, dtype=np.uint8)
 
-    video_sessions = get_video_sessions(should_have_marked_video=True)
+    if video_sessions is None:
+        video_sessions = get_video_sessions(should_have_marked_cells=True)
+    assert all([vs.has_marked_cells for vs in video_sessions]), 'Not all video sessions have marked cells.'
+
     if v:
         print('Creating cell and no cell images from videos and cell positions csvs...')
     for session in tqdm.tqdm(video_sessions):
@@ -119,6 +123,7 @@ def create_cell_and_no_cell_patches(
 def create_dataset_from_cell_and_no_cell_images(
         cell_images,
         non_cell_images,
+        validset_ratio=0.2,
         standardize=False,
         to_grayscale=False,
         device='cuda',
@@ -138,7 +143,7 @@ def create_dataset_from_cell_and_no_cell_images(
     if v:
         print('Splitting into training set and validation set')
 
-    trainset_size = int(len(dataset) * 0.80)
+    trainset_size = int(len(dataset) * (1 - validset_ratio))
     validset_size = len(dataset) - trainset_size
     # noinspection PyUnresolvedReferences
     trainset, validset = torch.utils.data.random_split(dataset, (trainset_size, validset_size))
@@ -148,6 +153,7 @@ def create_dataset_from_cell_and_no_cell_images(
 
 def get_cell_and_no_cell_patches(patch_size=(21, 21),
                                  n_negatives_per_positive=1,
+                                 video_sessions=None,
                                  normalise_patches=False,
                                  do_hist_match=False,
                                  standardize_dataset=False,
@@ -163,6 +169,9 @@ def get_cell_and_no_cell_patches(patch_size=(21, 21),
         created and if not then it creates it and saves it in cache.
 
     Args:
+        video_sessions (list[VideoSessions]):
+            The list of video sessions to use. If None automatically uses all video sessions available from
+            the data folder.
         normalise_patches: The cell and non cell images are normalised to 0 - 255
         patch_size (int, tuple): The patch size (height, width) or int for square.
         n_negatives_per_positive (int):  How many non cells per cell patch.
@@ -294,6 +303,7 @@ def get_cell_and_no_cell_patches(patch_size=(21, 21),
 
         cell_images, non_cell_images, cell_images_marked, non_cell_images_marked = \
             create_cell_and_no_cell_patches(patch_size=patch_size,
+                                            video_sessions=video_sessions,
                                             do_hist_match=False,
                                             n_negatives_per_positive=1,
                                             temporal_width=temporal_width,

learningutils.py
@@ -6,18 +6,64 @@ import numpy as np
 from torch.utils.data import Dataset
 
 
-class LabeledImageDataset(torch.utils.data.Dataset):
+class ImageDataset(torch.utils.data.Dataset):
+    """ Used to create a DataLoader compliant Dataset for binary classifiers
+    """
+    def __init__(self, images, standardize=True, to_grayscale=False, device='cuda'):
+        """
+        Args:
+          images (ndarray):
+              The images.
+              shape -> N x height x width x channels or N x height x width. dtype should be uint8.
+
+        """
+        assert device in ['cuda', 'cpu'], f'Device must be  one of {["cuda", "cpu"]}'
+        self.device = device
+
+        #  Handle images ( self.images should be NxHxWxC even when C is 1, and image type should be uint8
+        assert len(images.shape) == 3 or len(images.shape) == 4, \
+            f'Expected images shape to be one of NxHxWxC or NxHxW. Shape given {images.shape}.'
+        assert images.dtype == np.uint8, f'Images expected to have type uint8, not {images.dtype}.'
+        assert 3 <= len(images.shape) <= 4, f'The images should be an array of shape N x H x W x [ C ] not {images.shape}'
+        if len(images.shape) == 3:
+            #  NxHxW  -> NxHxWx1
+            images = images[..., None]
+        self.n_images, self.height, self.width, self.n_channels = images.shape
+        self.images = images
+
+        # Handle transforms
+        transforms = []
+        if self.n_channels > 1 and to_grayscale:
+            # Takes ndarray input with shape HxWxC
+            transforms.append(torchvision.transforms.ToPILImage())
+            # Grayscale takes a PILImage as an input
+            transforms.append(torchvision.transforms.Grayscale(num_output_channels=1))
+        # ToTensor accept uint8 [0, 255] numpy image of shape H x W x C and scales to [0, 1]
+        transforms.append(torchvision.transforms.ToTensor())
+        if standardize:
+            # Standardization brings values to -1 and 1
+            # Normalise takes input a tensor image of shape CxHxW and brings to target mean and standard deviation
+            transforms.append(torchvision.transforms.Normalize(mean=[0.5], std=[0.5]))
+        self.transform = torchvision.transforms.Compose(transforms)
+
+    def __len__(self):
+        return len(self.images)
+
+    def __getitem__(self, idx):
+        image = self.images[idx].copy()
+
+        if self.transform and not torch.is_tensor(image):
+            image = self.transform(image)
+
+        return image
+
+
+class LabeledImageDataset(ImageDataset):
     """ Used to create a DataLoader compliant Dataset for binary classifiers
     """
     samples: List[Tuple[np.ndarray, int]]
 
-    def __init__(self,
-                 images,
-                 labels,
-                 to_grayscale=False,
-                 standardize=False,
-                 device='cuda'
-                 ):
+    def __init__(self, images, labels, to_grayscale=False, standardize=False, device='cuda'):
         """
         Args:
           images (ndarray):
@@ -27,109 +73,22 @@ class LabeledImageDataset(torch.utils.data.Dataset):
           labels (ndarray):
               The corresponding list of labels.
         """
-        assert len(images.shape) == 3 or len(images.shape) == 4,\
-            f'Expected images shape to be one of NxHxWxC or NxHxW. Shape given {images.shape}.'
-        assert images.dtype == np.uint8, f'Images expected to have type uint8, not {images.dtype}.'
-        assert len(images) == len(labels),\
-            f'Expected to have equal amount of labels and images. n images:{len(images)} n labels:{len(labels)}'
+        super().__init__(images, standardize, to_grayscale, device)
+
         # if labels already ndarray nothing changes, if list makes to a numpy array
         labels = np.array(labels).squeeze()
+        assert len(images) == len(labels), \
+            f'Expected to have equal amount of labels and images. n images:{len(images)} n labels:{len(labels)}'
         assert labels.dtype == np.int, f'Labels must be integers not {labels.dtype}'
         assert len(labels.shape) == 1, f'Labels should be a list of one label for each image, shape given {labels.shape}'
-        assert device in ['cuda', 'cpu'], f'Device must be  one of {["cuda", "cpu"]}'
-        self.device = device
 
-        if len(images.shape) == 3:
-            images = images[..., np.newaxis]
-
-        self.n_images = images.shape[0]
-        self.n_channels = images.shape[-1]
-
-        if len(images.shape) == 3:
-            # if shape is NxHxW -> NxHxWx1
-            images = images[..., np.newaxis]
-
-        labels = torch.from_numpy(labels).to(device).type(torch.LongTensor)
-        self.samples = [(image, label) for image, label in zip(images, labels)]
-
-        transforms = []
-        if self.n_channels > 1 and to_grayscale:
-            # Takes ndarray input with shape HxWxC
-            transforms.append(torchvision.transforms.ToPILImage())
-            # Grayscale takes a PILImage as an input
-            transforms.append(torchvision.transforms.Grayscale(num_output_channels=1))
-
-        # ToTensor accept uint8 [0, 255] numpy image of shape H x W x C and scales to [0, 1]
-        transforms.append(torchvision.transforms.ToTensor())
-
-        if standardize:
-            # Standardization brings values to -1 and 1
-            # Normalise takes input a tensor image of shape CxHxW and brings to target mean and standard deviation
-            transforms.append(torchvision.transforms.Normalize(mean=[0.5], std=[0.5]))
-
-        self.transform = torchvision.transforms.Compose(transforms)
-
-    def __len__(self):
-        return len(self.samples)
+        self.labels = torch.from_numpy(labels).to(self.device).type(torch.LongTensor)
 
     def __getitem__(self, idx):
-        img, target = self.samples[idx]
+        img = self.images[idx]
+        label = self.labels[idx]
 
         if self.transform:
             img = self.transform(img)
 
-        return img.to(self.device), target
-
-
-class ImageDataset(torch.utils.data.Dataset):
-    """ Used to create a DataLoader compliant Dataset for binary classifiers
-    """
-    def __init__(self, images, standardize=True, to_grayscale=False):
-        """
-        Args:
-          images (ndarray):
-              The images.
-              shape -> N x height x width x channels or N x height x width. dtype should be uint8.
-
-        """
-        assert 3 <= len(images.shape) <= 4, f'The images should be an array of shape N x H x W x [ C ] not {images.shape}'
-
-        if len(images.shape) == 3:
-            #  NxHxW  -> NxHxWx1
-            images = images[..., None]
-
-        self.n_images, self.height, self.width, self.n_channels = images.shape
-
-        transforms = []
-        if self.n_channels > 1 and to_grayscale:
-            # ToPILImage Takes ndarray input with shape HxWxC
-            transforms.append(torchvision.transforms.ToPILImage())
-            # Grayscale takes a PILImage as an input
-            transforms.append(torchvision.transforms.Grayscale(num_output_channels=1))
-
-        # ToTensor accept uint8 [0, 255] numpy image of shape H x W x C and scales to [0, 1]
-        transforms.append(torchvision.transforms.ToTensor())
-
-        if standardize:
-            # Standardization brings values to -1 and 1
-            # Normalise takes input a tensor image of shape CxHxW and brings to target mean and standard deviation
-            transforms.append(torchvision.transforms.Normalize(mean=[0.5], std=[0.5]))
-
-        if standardize:
-            transforms.append(torchvision.transforms.Normalize([0.5], [0.5]))
-
-        self.samples = images
-        self.transform = torchvision.transforms.Compose(transforms)
-
-    def __len__(self):
-        return len(self.samples)
-
-    def __getitem__(self, idx):
-        image = self.samples[idx].copy()
-
-        if self.transform and not torch.is_tensor(image):
-            image = self.transform(image)
-
-        return image
-
-
+        return img.to(self.device), label

measure_velocity.py
@@ -47,7 +47,7 @@ def get_positions_from_csv(csv_file, frame_idx):
     return all_cell_positions[np.where(all_cell_frame_indices == frame_idx)[0]]
 
 
-video_sessions = get_video_sessions(should_have_marked_video=True)
+video_sessions = get_video_sessions(should_have_marked_cells=True)
 # keep only the video sessions that have vessel masks
 video_sessions = [vs for vs in video_sessions if vs.vessel_mask_confocal_file and vs.vessel_mask_oa850_file]
 
@@ -396,4 +396,4 @@ def create_average_images(video_session, patch_size=51, sigma=1, average_all_fra
 
 
 if __name__ == '__main__':
-    create_average_images(get_video_sessions(should_have_marked_video=True)[2], patch_size=51, sigma=0.2, average_all_frames=False)
+    create_average_images(get_video_sessions(should_have_marked_cells=True)[2], patch_size=51, sigma=0.2, average_all_frames=False)